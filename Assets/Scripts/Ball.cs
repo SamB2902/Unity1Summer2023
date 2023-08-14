aa909f740499fe5878a0c98e066d3fa0ef59f888
SamB2902 <sebianchi@email.neit.edu>
2023-08-14 13:50:08 -0400

    things

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -14,8 +14,9 @@ public class Ball : MonoBehaviour
     [SerializeField]
     GameObject gameOverHud;
     public TimerScript clock;
-    public AudioClip sfx;
+    public AudioClip[] sfx;
     AudioSource soundSource;
+    public AudioClip endSound;
 
     // Start is called before the first frame update
     void Start()
@@ -31,12 +32,18 @@ public class Ball : MonoBehaviour
     {
        if(transform.position.y < -10)
         {
+           
+            if(!clock.isGameOver)
+            {
+                soundSource.PlayOneShot(endSound);
+            }
+
             clock.isGameOver = true;
             gameOverHud.SetActive(true);
             Cursor.visible = true;
 
             //highscore stuff
-            if(score > highScore)
+            if (score > highScore)
             {
                 highScore = score;
                 PlayerPrefs.SetInt("highScore", highScore);
@@ -58,7 +65,7 @@ public class Ball : MonoBehaviour
     {
         if(collision.gameObject.tag == "Paddle")
         {
-            soundSource.PlayOneShot(sfx);
+            soundSource.PlayOneShot(sfx[Random.Range(0,2)]);
             score++;
             //uodate the score text
             scoreTxt.text = $"Score: {score}";
