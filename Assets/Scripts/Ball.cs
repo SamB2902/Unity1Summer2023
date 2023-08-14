1298862eae9a5f4870d52b9941f1bc26a51b32e6
SamB2902 <sebianchi@email.neit.edu>
2023-08-14 11:28:43 -0400

    stuff

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
     Vector3 startPos;
     Rigidbody rb;
+    [SerializeField]
+    TMP_Text scoreTxt;
+    int score;
+    int highScore = 0;
+    [SerializeField]
+    GameObject gameOverHud;
+    public TimerScript clock;
+    public AudioClip sfx;
+    AudioSource soundSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         startPos = transform.position;
+        gameOverHud.SetActive(false);
+        soundSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,8 +31,48 @@ public class Ball : MonoBehaviour
     {
        if(transform.position.y < -10)
         {
-            transform.position = startPos;
-            rb.velocity = Vector3.zero;
+            clock.isGameOver = true;
+            gameOverHud.SetActive(true);
+            Cursor.visible = true;
+
+            //highscore stuff
+            if(score > highScore)
+            {
+                highScore = score;
+                PlayerPrefs.SetInt("highScore", highScore);
+            }
+
+            if(clock.seconds > 9)
+            {
+                gameOverHud.GetComponent<TMP_Text>().text = $"Game Over \nYour high score is {PlayerPrefs.GetInt("highScore")} \nYour time was: {clock.minutes}:{clock.seconds}";
+            }
+            else
+            {
+                gameOverHud.GetComponent<TMP_Text>().text = $"Game Over \nYour high score is {PlayerPrefs.GetInt("highScore")} \nYour time was: {clock.minutes}:0{clock.seconds}";
+            }
         }
+    }
+
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Paddle")
+        {
+            soundSource.PlayOneShot(sfx);
+            score++;
+            //uodate the score text
+            scoreTxt.text = $"Score: {score}";
+        }
+    }
+    public void RestartGame()
+    {
+        gameOverHud.SetActive(false);
+        transform.position = startPos;
+        rb.velocity = Vector3.zero;
+        score = 0;
+        clock.timeStart = 0;
+        clock.isGameOver = false;
+        scoreTxt.text = $"Score: {score}";
+        Cursor.visible = false;
     }
 }
